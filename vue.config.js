ae552ee5fe8bee829210cc46b6203d4e0aaf4aa4
xiaoming.bai <80583600@qq.com>
2020-10-07 14:06:32 +0800

    webpack-dev-server

--- vue.config.js
+++ vue.config.js
@@ -8,6 +8,22 @@ const path = require('path')
 module.exports = {
   pluginOptions: {
     /**
+     * webpack-dev-server
+     * https://webpack.docschina.org/configuration/dev-server/
+     */
+    devServer: {
+      proxy: {
+        '/api': {
+          target: 'http://localhost:4000',
+          ws: true,
+          changeOrigin: true,
+        },
+        '/foo': {
+          target: 'http://localhost:4001',
+        },
+      },
+    },
+    /**
      * vue-cli-plugin-style-resources-loader
      * https://github.com/nguyenvanduocit/vue-cli-plugin-style-resources-loader
      */
