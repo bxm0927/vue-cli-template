5bf3dd71e4335304ec3663cc8b030c6c3fb2988a
xiaoming.bai <80583600@qq.com>
2020-10-10 11:28:56 +0800

    在没有匹配到路由的时候返回 404 页面

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,6 +4,7 @@ import Home from '../views/Home.vue'
 // this generates a separate chunk (about.[hash].js) for this route
 // which is lazy-loaded when the route is visited.
 const About = () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+const NotFound = () => import(/* webpackChunkName: "notFound" */ '../views/NotFound.vue')
 
 const routes = [
   {
@@ -15,6 +16,11 @@ const routes = [
     path: '/about',
     name: 'About',
     component: About,
+  },
+  // 在没有匹配到路由的时候返回 404 页面
+  {
+    path: '*',
+    component: NotFound,
   },
 ]
 
---
+++ src/views/NotFound.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="page">
+    <h1>404, Page Not Found!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NotFound',
+}
+</script>
